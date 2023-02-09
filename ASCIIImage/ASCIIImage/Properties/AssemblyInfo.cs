5601de2e6b70e13e38b68b1135127798f81468c9
Veteranfighter <63098858+Veteranfighter@users.noreply.github.com>
2023-02-09 21:13:57 +0100

    edit assemblyinfo

--- ASCIIImage/ASCIIImage/Properties/AssemblyInfo.cs
+++ ASCIIImage/ASCIIImage/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ASCIIImage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Lenge")]
 [assembly: AssemblyProduct("ASCIIImage")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
